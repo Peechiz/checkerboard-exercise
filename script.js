771e1e54f5680c1bf100a8008a7ab126090e39e1
Chris Impicciche <peechiz@bakudan.local>
2016-05-08 12:14:21 -0500

    adds color gradient

--- script.js
+++ script.js
@@ -4,22 +4,36 @@ console.log('hello there');
 
 //create divs
 
-function randoColor(){
-  var r,g,b;
-  r = Math.floor(Math.random()*256);
-  g = Math.floor(Math.random()*256);
-  b = Math.floor(Math.random()*256);
+var shade1,shade2;
+shade1 = 0;
+shade2 = 0;
 
-  color = 'rgb(' + r + ',' + g + ',' + b +')';
-
-  return color;
+makeColor = function(shade,range){
+  var str;
+  if (range === 'r'){
+    str = 'rgb('+shade+',0,0)'
+  }
+  else if (range === 'g'){
+    str = 'rgb(0,'+shade+',0)'
+  }
+  else if (range === 'b'){
+    str = 'rgb(0,0,'+shade+')'
+  }
+  console.log(str);
+  return str;
 }
 
-console.log(randoColor());
-
 for (var i=0; i<81; i++){
+
   var div = document.createElement('div')
   div.style.cssText = 'width: 11.1%; padding-bottom: 11.1%; float: left;';
-  div.style.backgroundColor = randoColor();
+  if (i%2==0){
+    div.style.backgroundColor = makeColor(shade1,'r');
+    shade1 += 5;
+  }
+  else {
+    div.style.backgroundColor = makeColor(shade2,'g');
+    shade2+=5;
+  }
   document.body.appendChild(div);
 }
