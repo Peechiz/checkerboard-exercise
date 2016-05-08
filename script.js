2bbcc07e769b88e3e641d584d32c9d337a38e995
Chris Impicciche <peechiz@bakudan.local>
2016-05-08 11:48:56 -0500

    changes board to randomcolors

--- script.js
+++ script.js
@@ -4,14 +4,22 @@ console.log('hello there');
 
 //create divs
 
+function randoColor(){
+  var r,g,b;
+  r = Math.floor(Math.random()*256);
+  g = Math.floor(Math.random()*256);
+  b = Math.floor(Math.random()*256);
+
+  color = 'rgb(' + r + ',' + g + ',' + b +')';
+
+  return color;
+}
+
+console.log(randoColor());
+
 for (var i=0; i<81; i++){
   var div = document.createElement('div')
   div.style.cssText = 'width: 11.1%; padding-bottom: 11.1%; float: left;';
-  if (i%2==0){
-    div.style.backgroundColor = 'red';
-  }
-  else {
-    div.style.backgroundColor = 'black';
-  }
+  div.style.backgroundColor = randoColor();
   document.body.appendChild(div);
 }
